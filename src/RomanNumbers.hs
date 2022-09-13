76aebba017081bf3c5bbeda2217c31e0627599bc
David <d.v.r.chen@student.tue.nl>
2022-09-13 15:43:40 +0200

    Added alternative version of i2r (i2r').

--- src/RomanNumbers.hs
+++ src/RomanNumbers.hs
@@ -38,7 +38,24 @@ i2r :: Int -> String
 -- i2r n = 1
 i2r n = strmult (n `div` 1000) "M" ++ strmult ((n `mod` 1000) `div` 500) "D" ++ strmult ((n `mod` 500) `div` 100) "C" ++ strmult ((n `mod` 100) `div` 50) "L" ++ strmult ((n `mod` 50) `div` 10) "X" ++ strmult((n `mod` 10) `div` 5) "V" ++ strmult(n `mod` 5) "I"
 
+i2r' :: Int -> String
+i2r' = gi2r 1
 
+gi2r :: (Eq a, Num a) => a -> Int -> [Char]
+gi2r z 0 = ""
+gi2r z i = strmult q a ++ gi2r (z + 1) r
+  where (a,b) = cnt z
+        (q,r) = divMod i b
+
+cnt :: (Eq a, Num a, Num b) => a -> ([Char], b)
+cnt 1 = ("M", 1000)
+cnt 2 = ("D", 500)
+cnt 3 = ("C", 100)
+cnt 4 = ("L", 50)
+cnt 5 = ("X", 10)
+cnt 6 = ("V", 5)
+cnt 7 = ("I", 1)
+cnt _ = error "This should not happen?!"
 
 
 -- | Implements repeating a string (or any list) as would be expected from e.g. python. 
